d394f32007993a61466eb931e0dfd82f38d9f2b4
2172772 <2172772@cegepmontpetit.ca>
2024-04-13 18:22:46 -0400

    Bug in enemy fonction

--- Assets/Scripts/CameraSmoothCarFollow.cs
+++ Assets/Scripts/CameraSmoothCarFollow.cs
@@ -10,12 +10,17 @@ public class CameraSmoothCarFollow : MonoBehaviour
     [SerializeField] float Speed;
     private void Start()
     {
+        // Cela déplace la position de la caméra de manière fluide vers la position de CameraPlace.
         CameraPlace = GameObject.FindGameObjectWithTag("CameraFollow").transform;
     }
     void Update()
     {
+        //pour un mouvement doux et naturel. La variable Speed contrôle la vitesse du mouvement.  
         transform.position = Vector3.SmoothDamp(transform.position, CameraPlace.position, ref ccurrent, Speed * Time.deltaTime,50);
+        // la caméra regarde toujours vers l'objet Car.
         transform.LookAt(Car);
     }
+    //C'est une variable pour stocker la vitesse actuelle de déplacement de la caméra,
+    //utilisée par Vector3.SmoothDamp. 
     Vector3 ccurrent;
 }
